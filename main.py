60ac2f7ba180b0581771ee7cd7cc1c6a8d78b1ff
james <james.richard.hemmings@gmail.com>
2022-07-07 12:19:07 -0700

    separate footer and header into their own files, make year in copyright dynamically update.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from flask import Flask,render_template
 import os
+from datetime import datetime
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
+year = datetime.today().year
+
+@app.context_processor
+def inject_year():
+    return dict(year=year)
 
 @app.route('/')
 def home():
